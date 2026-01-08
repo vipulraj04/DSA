2d2c20279bc7fc3164d1f869168a9d8d7ec38ac6
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-01-08 18:17:06 +0530

    Time: 9 ms (82.39%) | Memory: 15.4 MB (89.84%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int>ans;
+        if(root==nullptr){
+            return ans;
+        }
+
+        traversal(root,ans);
+
+        return ans;
+    }
+
+    void traversal(Node*root,vector<int>&ans){
+        if(root==nullptr){
+            return;
+        }
+
+        ans.push_back(root->val);
+
+        for(int i=0;i<root->children.size();i++){
+            traversal(root->children[i],ans);
+        }
+    }
+};
