661c8398ecb1e71e31efb5be13c90c0ada71366e
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-01-20 18:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (12.43%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
+        int n=nums.size();
+
         vector<int>result;
-        for(int num : nums){
-            if(num % 2==0){
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==2){
                 result.push_back(-1);
                 continue;
             }
-            for(int x=0;x<num;x++){
-                if((x|x+1)==num){
-                    result.push_back(x);
-                    break;
+
+            for(int j=0;j<=32;j++){
+                if((nums[i] & (1<<j))>0){
+                    continue;
                 }
+
+                int prev=j-1;
+                int x= nums[i] ^ (1<<(j-1));
+
+                result.push_back(x);
+                break;
             }
         }
 
         return result;
-        
     }
 };
