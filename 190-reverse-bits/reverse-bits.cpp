d6e6e8a6b6ac57a590dc4b97aca83030df9c70f2
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-02-16 17:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (73.18%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        unsigned long long result=0;
-        for(int i=0;i<32;i++){
-            long long lastBit=n%2;
-
-            result=result*2+lastBit;
-
-            n=n/2;
+        if(n==0){
+            return 0;
         }
+        int result=0;
+        for(int i=1;i<=32;i++){
+            result<<=1;
 
-        return (int)result;
-        
+            result=(result|(n&1));
+
+            n>>=1;
+        }
+        return result;
     }
 };
