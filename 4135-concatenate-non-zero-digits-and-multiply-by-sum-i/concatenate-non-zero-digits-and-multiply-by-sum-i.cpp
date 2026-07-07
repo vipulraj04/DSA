8779c9996005113affd43d092bacff1e845ce7a4
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-07-07 10:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (27.38%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
+vector<int> getDigit(int n){
+    vector<int>result;
+    while(n){
+        result.push_back(n%10);
+        n=n/10;
+    }
+    return result;
+}
     long long sumAndMultiply(int n) {
-        long long sum=0;
+        vector<int>result=getDigit(n);
         long long newNumber=0;
-        string s= to_string(n);
-        for(char c: s){
-            int digit=c-'0';
-            sum+=digit;
-            if(digit >0){
-                newNumber=newNumber*10+digit;
+        long long sum=0;
+        for(int i=result.size()-1;i>=0;i--){
+            sum+=result[i];
+            if(result[i]>0){
+                newNumber=newNumber*10+result[i];
             }
         }
-
         return newNumber*sum;
     }
 };
