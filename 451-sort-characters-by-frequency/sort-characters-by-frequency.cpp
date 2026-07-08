b288434b2809ed9ed99c7c85f6fa058c59152f14
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-07-08 11:41:37 +0530

    Time: 3 ms (54.14%) | Memory: 11.5 MB (28.43%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        vector<pair<char,int>>result(256);
+        for(char ch : s){
+            result[(unsigned char)ch].first=ch;
+            result[(unsigned char)ch].second++;
+        }
+        auto lambda=[](pair<char,int>&p1,pair<char,int>&p2){
+            return p1.second > p2.second;
+        };
+
+        sort(result.begin(),result.end(),lambda);
+
+        string ans="";
+        for(auto & it: result){
+            char ch=it.first;
+            int freq=it.second;
+            if(freq==0){
+                continue;
+            }
+            //string temp=string()
+            ans+=string(freq,ch);
+        }
+        return ans;
+    }
+};
