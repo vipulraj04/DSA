2ff076634a1a46a5154810d0b84b846c4fd29527
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-08-13 23:16:42 +0530

    Time: 36 ms (20.81%) | Memory: 23.9 MB (76.87%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-bool isValid(int nr,int nc,int n,int m,vector<vector<int>>&grid,
-vector<vector<int>>&dist){
-    return (nr>=0 && nr < n && nc>=0 && nc < m && grid[nr][nc]==0 &&
-    dist[nr][nc]==INT_MAX);
+bool isValid(int nr,int nc,int n,int m,vector<vector<int>>& grid,vector<vector<int>>&dist){
+    return (nr>=0 && nc >=0 && nr < n && nc < m && grid[nr][nc]==0 && dist[nr][nc]==INT_MAX);
 }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
@@ -12,32 +10,35 @@ vector<vector<int>>&dist){
         if(grid[0][0]==1 || grid[n-1][m-1]==1){
             return -1;
         }
-        vector<vector<int>>dir={{-1,0},{1,0},{0,1},{0,-1},{-1,-1},{1,1},{-1,1},{1,-1}};
-
-        queue<pair<int,int>>q;
-
+        vector<vector<int>>dir={{-1,0},{1,0},{0,1},{0,-1},{-1,-1},{+1,+1},{-1,1},{1,-1}};
         vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-        q.push({0,0});
         dist[0][0]=1;
-        while(!q.empty()){
-            int i=q.front().first;
-            int j=q.front().second;
-            q.pop();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+
+        pq.push({1,{0,0}});
+
+        while(!pq.empty()){
+            int ct=pq.top().first;
+            int i=pq.top().second.first;
+            int j=pq.top().second.second;
+
+            pq.pop();
 
             for(auto & it : dir){
                 int nr=i+it[0];
                 int nc=j+it[1];
 
                 if(isValid(nr,nc,n,m,grid,dist)){
-                    dist[nr][nc]=dist[i][j]+1;
-                    q.push({nr,nc});
+                    if(ct  + 1 < dist[nr][nc]){
+                        dist[nr][nc]= ct+1;
+                        pq.push({ct+1,{nr,nc}});
+                    } 
                 }
             }
         }
         if(dist[n-1][m-1]==INT_MAX){
             return -1;
         }
-
         return dist[n-1][m-1];
     }
 };
