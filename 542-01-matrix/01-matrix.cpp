b5f3577ff7fa8e68fc16be614056afca644edca8
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-09-01 22:49:06 +0530

    Time: 170 ms (5.02%) | Memory: 74.9 MB (5.04%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
 bool isValid(int i,int j,int n,int m){
-    if(i<0 || j<0 || i>=n || j>=m){
-        return false;
-    }
-    return true;
+    return (i>=0 && j>=0 && i<n && j<m );
 }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-
-        vector<vector<bool>>visited(n,vector<bool>(m,false));
         vector<vector<int>>result(n,vector<int>(m,-1));
-        queue<pair<pair<int,int>,int>>q;
+        queue<pair<int,int>>q;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    q.push({{i,j},0});
-                    visited[i][j]=true;
+                    result[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
+        vector<vector<int>>dir={
+            {-1,0},{1,0},{0,-1},{0,1}
+        };
+
         while(!q.empty()){
-            int i=q.front().first.first;
-            int j=q.front().first.second;
-            int time=q.front().second;
+            int i=q.front().first;
+            int j=q.front().second;
+
             q.pop();
 
-            result[i][j]=time;
+            for(auto d: dir){
+                int nx=i+d[0];
+                int ny=j+d[1];
 
-            if(isValid(i+1,j,n,m) && !visited[i+1][j]){
-                visited[i+1][j]=true;
-                q.push({{i+1,j},time+1});
-            }
-
-            if(isValid(i-1,j,n,m) && !visited[i-1][j]){
-                visited[i-1][j]=true;
-                q.push({{i-1,j},time+1});
-            }
-
-            if(isValid(i,j+1,n,m) && !visited[i][j+1]){
-                visited[i][j+1]=true;
-                q.push({{i,j+1},time+1});
-            }
-            if(isValid(i,j-1,n,m) && !visited[i][j-1]){
-                visited[i][j-1]=true;
-                q.push({{i,j-1},time+1});
+                if(isValid(nx,ny,n,m) && result[nx][ny]==-1){
+                    result[nx][ny]=result[i][j]+1;
+                    q.push({nx,ny});
+                }
             }
         }
 
