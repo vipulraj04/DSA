d79783f5366338849ae03bfe02ae084e44cff22f
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-09-09 01:56:57 +0530

    Time: 3 ms (63.15%) | Memory: 17.7 MB (73.79%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>>adj(n);
-        for(auto & it : flights){
+        for(auto & it: flights){
             int u=it[0];
             int v=it[1];
             int ct=it[2];
@@ -10,25 +10,24 @@ public:
             adj[u].push_back({v,ct});
         }
         queue<pair<int,pair<int,int>>>q;
+        q.push({0,{src,0}});
         vector<int>dist(n,INT_MAX);
         dist[src]=0;
-        q.push({0,{src,0}});
         while(!q.empty()){
             int stop=q.front().first;
             int node=q.front().second.first;
-            int wt=q.front().second.second;
-
+            int ct=q.front().second.second;
             q.pop();
 
             if(stop > k){
                 continue;
             }
 
-            for(auto & it:adj[node]){
+            for(auto &it:adj[node]){
                 int nNode=it.first;
-                int ct=it.second;
+                int wt=it.second;
 
-                if(ct+wt < dist[nNode] && stop <=k){
+                if(ct + wt < dist[nNode] && stop <=k){
                     dist[nNode]=ct+wt;
                     q.push({stop+1,{nNode,ct+wt}});
                 }
@@ -37,6 +36,7 @@ public:
         if(dist[dst]==INT_MAX){
             return -1;
         }
+
         return dist[dst];
     }
 };
